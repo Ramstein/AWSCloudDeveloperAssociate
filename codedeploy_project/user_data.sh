522f455696ce2d11d12342384718aaef4e356b19
MD.Zeeshan <gerialworld@gmail.com>
2022-04-03 22:58:20 +0000

    new updates

--- codedeploy_project/user_data.sh
+++ codedeploy_project/user_data.sh
@@ -2,13 +2,13 @@
 su ec2-user
 sudo yum install httpd -y
 sudo chown ec2-user:ec2-user -R /var/www
-cat > /var/www/html/index.html <<EOL
+sudo cat > /var/www/html/index.html <<EOL
 <html>
   <head>
-    <title>My CodeDeploy WebApp</title>
+    <title>My CodeDeploy WebApp from revision</title>
   </head>
   <body>
-     <h1>My CodeDeploy WebApp body</h1>
+     <h1>My CodeDeploy WebApp body from revision</h1>
   </body>
 </html>
 EOL
