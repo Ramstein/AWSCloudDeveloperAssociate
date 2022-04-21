faa9a5992870693df79b551ac4a1e99863071131
MD.Zeeshan <gerialworld@gmail.com>
2022-04-21 08:57:52 +0000

    Add files via upload

--- codedeploy_project/user_data.sh
+++ codedeploy_project/user_data.sh
@@ -4,12 +4,12 @@ sudo yum install httpd -y
 sudo chown ec2-user:ec2-user -R /var/www
 sudo cat > /var/www/html/index.html <<EOL
 <html>
-  <head>
-    <title>My CodeDeploy WebApp from revision</title>
-  </head>
-  <body>
-     <h1>My CodeDeploy WebApp body from revision</h1>
-  </body>
+    <head>
+        <title>My CodeDeploy app</title>
+    </head>
+    <body>
+        <h1>My Codedeploy App v1</h1>
+    </body>
 </html>
 EOL
 sudo systemctl start httpd
